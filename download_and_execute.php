3eba58f6cfddc23b31e920d755ed0a5a44ae778b
suriya1201 <2001375A@student.tp.edu.sg>
2024-06-21 20:18:46 +0800

    Update download_and_execute.php

--- download_and_execute.php
+++ download_and_execute.php
@@ -1,6 +1,6 @@
 <?php
 // Set the file path
-$filePath = '"C:\Users\suriya\Documents\xampp\htdocs\sussiest.exe"sussiest.exe'; // Replace 'path_to_your_executable_file.exe' with the actual path to your executable file
+$filePath = 'C:\Users\suriya\Documents\xampp\htdocs\sussiest.exe'; // Replace 'path_to_your_executable_file.exe' with the actual path to your executable file
 
 // Content-Disposition: inline; filename="file.exe"
 header('Content-Disposition: attachment; filename="sussiest.exe"');
